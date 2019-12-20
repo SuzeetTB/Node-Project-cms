a7cc02c1bfc6c972d2b5d494b9a84c810cad28e1
SuzeetTB <su2e3t@live.com>
2019-12-20 11:09:19 +0545

    first Commit

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports={
     //this is the local database
-    //mongoDbUrl:'mongodb://localhost:27017/cms',
+    mongoDbUrl:'mongodb://localhost:27017/cms',
 
     //this is remote database
-    mongoDbUrl:'mongodb+srv://Suzeet:zxc123@cluster0-xn6iq.mongodb.net/cms?retryWrites=true&w=majority'
+    //mongoDbUrl:'mongodb+srv://Suzeet:zxc123@cluster0-xn6iq.mongodb.net/cms?retryWrites=true&w=majority'
 
 };
